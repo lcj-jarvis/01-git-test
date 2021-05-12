db9f6cc1a27a91cce337f576b601873bf812bfac
简单de快乐 <1802772962@qq.com>
2021-05-12 18:53:10 +0800

    first commit

---
+++ src/main/java/com/mrlu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.mrlu.git;
+
+/**
+ * @author 简单de快乐
+ * @date 2021-05-12 18:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
